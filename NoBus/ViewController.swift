42612271d6f88dcb23dc75e6be37e4e5714ef57d
leavez <gaojiji@gmail.com>
2018-10-24 23:24:11 +0800

    initial commint

---
+++ NoBus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoBus
+//
+//  Created by Gao on 2018/10/24.
+//  Copyright © 2018 me.leavez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
