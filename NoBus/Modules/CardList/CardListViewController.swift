745185f1da57a1ed87c2d78a751bc78f5ce67f92
leavez <gaojiji@gmail.com>
2018-11-25 15:44:39 +0800

    add setting entrance view

--- NoBus/Modules/CardList/CardListViewController.swift
+++ NoBus/Modules/CardList/CardListViewController.swift
@@ -37,11 +37,12 @@ class CardListViewController: UIViewController {
         )
         collectionView.style { (collectionView) in
             collectionView.register(StationCardCell.self, forCellWithReuseIdentifier: "cell")
+            collectionView.register(SettingCell.self, forCellWithReuseIdentifier: SettingCell.settingIdentifier)
             let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
             layout.sectionInset = UIEdgeInsets(top: 30, left: 30, bottom: 30, right: 30)
             layout.estimatedItemSize = CGSize(width: 100, height: 100)
-            layout.minimumInteritemSpacing = 40
-            layout.minimumLineSpacing = 40
+            layout.minimumInteritemSpacing = 30
+            layout.minimumLineSpacing = 30
             collectionView.backgroundColor = .white // UIColor(red: 0.94, green: 0.94, blue: 0.96, alpha: 1)
             collectionView.alwaysBounceVertical = true
         }
@@ -50,12 +51,25 @@ class CardListViewController: UIViewController {
         
         let dataSource = RxCollectionViewSectionedReloadDataSource<ItemViewModel.Section> (configureCell: {
                 (datasource, collectionView, index, i) -> UICollectionViewCell in
+            if i.name == SettingCell.settingIdentifier {
+                return collectionView.dequeueReusableCell(withReuseIdentifier: SettingCell.settingIdentifier, for: index)
+            } else {
                 let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: index) as? StationCardCell
                 cell?.bind(viewModel: i)
                 return cell ?? UICollectionViewCell()
-            })
+            }
+        })
         
         viewModel.items.asObservable()
+            .map({
+                let fakeSettingSection: ItemViewModel.Section = {
+                    let setting = ItemViewModel.StationCell(stationName: SettingCell.settingIdentifier, lines: [])
+                    var section = ItemViewModel.Section()
+                    section.stations = [setting]
+                    return section
+                }()
+                return $0 + [fakeSettingSection]
+            })
             .bind(to: collectionView.rx.items(dataSource: dataSource))
             .disposed(by: bag)
 
@@ -74,8 +88,31 @@ class CardListViewController: UIViewController {
     }
     
   
+//    func settingEntrance() -> UIView {
+//        
+//    }
 
 }
 
+class SettingCell: StationCardCell {
+    static let settingIdentifier = "setting"
+    
+    let title = UIButton()
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.subviews.forEach {
+            $0.removeFromSuperview()
+        }
+        contentView.sv(title)
+        title.fillContainer(20)
+        title.setTitle("SETTING", for: .normal)
+        title.titleLabel?.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        title.setTitleColor(UIColor.table.subDescription.light, for: .normal)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
 
